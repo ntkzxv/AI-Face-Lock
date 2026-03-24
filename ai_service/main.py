f9e71d1dc52c5feee2c4e94f527c1c4c9a2a0335
IP <peacepeerapat@IPP.local>
2026-03-24 18:30:36 +0700

    fix:allow_origins

--- ai_service/main.py
+++ ai_service/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # เปิด CORS ให้ Next.js (Localhost) และที่อื่นๆ เข้าถึงได้
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
